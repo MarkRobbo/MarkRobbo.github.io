be9088fb83e1160c5668553f566a9da91007a307
Mark Robinson <mark@goldtek.co.uk>
2021-01-26 23:34:26 +0000

    Add simple holding page back to Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="content">
+      <h1 id="name">Mark Robinson</h1>
+      <h2 id="tagline">Software Engineer</h2>
+      <ul id="social">
+          <li class="first"><a href="https://www.linkedin.com/in/mark-robinson-8b4813109/" target="_blank"><img src="./assets/linkedin.svg" alt="LinkedIn" width="32" height="32"/></a></li>
+          <li><a href="https://www.github.com/MarkRobbo" target="_blank"><img src="./assets/github.png" alt="Github" width="32" height="32"/></a></li>
+          <li><a href="mailto:me@markrobinson.dev" target="_blank"><img src="./assets/gmail.svg" alt="Email" width="32" height="32"/></a></li>
+      </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+* {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background-color: #f9f9f9;
+  overflow-y: hidden;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 48px;
+}
+
+h1, h2 {
+  padding: 0;
+  margin: 0;
+}
+
+li.first {
+  margin-left: 3px !important;
+}
+
+#social {
+  list-style-type: none;
+  overflow: hidden;
+  padding: 0;
+}
+
+#social li {
+  float: left;
+  margin-left: 15px
+}
+
+#content {
+  width: 375px;
+  height: 200px;
+  padding: 20px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
 }
 </style>
