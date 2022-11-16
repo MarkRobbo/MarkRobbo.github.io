d62b19ab8c7ef3766b3f84703c8ce218706d775a
Mark Robinson <zmrobns@amazon.com>
2022-11-16 20:53:21 +0000

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag';
 
-createApp(App).mount('#app')
+createApp(App).use(VueGtag, {
+    config: { id: 'G-BX1NW8PG3H' },
+    pageTrackerScreenviewEnabled: true // I'm watching you 👀
+}).mount('#app')
